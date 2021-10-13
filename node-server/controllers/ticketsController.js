acce23a3c84219b657ae3a9396330029b595dfac
saddamrao2572 <saddamrao@gmail.com>
2021-10-13 13:49:14 -0700

    basci controllers

---
+++ node-server/controllers/ticketsController.js
@@ -0,0 +1,18 @@
+
+
+// Display list of all Locations.
+exports.location_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Locations list');
+};
+
+// Display list of all Locations.
+exports.get_all_locations = function(req, res) {
+    res.send('NOT IMPLEMENTED: Locations list idddddddd'+ req.params.id);
+};
+
+
+// Display list of all Locations.
+exports.index = function(req, res) {
+    res.send('Please Enter API Keys');
+};
+
